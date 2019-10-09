4043e2b6fa6d07627d66ffe0ac16472de634340b
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-10-08 19:48:48 -0700

    add db connection

---
+++ src/NotefulForm/NotefulForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NotefulForm.css';
+import PropTypes from 'prop-types';
+
+export default function NotefulForm(props) {
+  const { className, ...otherProps } = props
+  return (
+    <form
+      className={['Noteful-form', className].join(' ')}
+      action='#'
+      {...otherProps}
+    />
+  )
+}
+NotefulForm.propTypes = {
+	className: PropTypes.string,
+}
